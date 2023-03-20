27d16d77a59ef5094ea1c125ab309cb2fbc50aa5
claudio-sr <jclaudiosiqueira.js@gmail.com>
2023-03-20 11:30:22 -0300

    Conta terminal

--- conta-banco/src/Conta.java
+++ conta-banco/src/Conta.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Conta {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo = 237.48;
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o nome do cliente: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        agencia = sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        numeroConta = sc.nextInt();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponivel");
+
+
     }
 }
